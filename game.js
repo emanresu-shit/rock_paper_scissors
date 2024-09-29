60be4f8429b0a9f4f82c041fe774b6257b0b45c6
emanresu-shit <a91947881@gmail.com>
2024-09-29 21:00:03 +0300

    Add UI for the game

--- game.js
+++ game.js
@@ -1,13 +1,3 @@
-function getUserPlay() {
-    let playMode = prompt("what's your play? rock, paper or scissor");
-    playMode = playMode.toLowerCase();
-    if (!(playMode === "rock" || playMode === "paper" || playMode === "scissor")) {
-        return "";
-    }
-    return playMode
-}
-
-// now get computer play
 function getComputerPlay() {
     let playMode;
     let randNum = Math.floor(Math.random() * 100) + 1;
@@ -21,13 +11,17 @@ function getComputerPlay() {
     return playMode;
 }
 
-// now game magic
-function gamePlay() {
-    let userPlay = getUserPlay();
-    if (userPlay==""){
-        return 0;
-    } 
-    let computerPlay = getComputerPlay();
+
+const buttonList = document.querySelector(".container > .playing-area > .playing-buttons");
+
+function gamePlay(userPlayMode, computerPlayMode) {
+    // if (userPlay==""){
+    //     return 0;
+    // } 
+    // let computerPlay = getComputerPlay();
+    // 1 => player wins
+    // 2 => computer wins
+    // 3 => draw(invalid input)
 
     let gameResult;
     if (userPlay === "rock") {
@@ -44,23 +38,71 @@ function gamePlay() {
     if(userPlay == computerPlay){
         gameResult = 3;
     }
-    return gameResult;
+    return gameResult;   
 }
+
 let win=0, loss=0, draw=0;
-function playGame(){
-    let r = gamePlay()
+
+function playGame(userPlayMode, computerPlayMode){
+    let r = gamePlay(userPlayMode, computerPlayMode)
     if(r==0 || r==3) draw+=1; 
     if(r==1) win+=1;
     if(r==2) loss+=1;
 }
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
 
-if(win>loss){
-    console.log("you Won!");
-} else {
-    console.log("you lost!")
-}
+
+const status_text = document.querySelector(".status-title");
+const rounds = document.querySelector("#round");
+const humanScore = document.querySelector("#WIN");
+const drawScore = document.querySelector("#DRAW")
+const computerScore = document.querySelector("#LOSS")
+
+
+let rnd = 0;
+
+let userPlay, computerPlay, gameResult;
+buttonList.addEventListener("click", (event)=>{
+    let target = event.target;
+    if(rnd < 5){
+        status_text.textContent = "";
+    }
+    
+    switch(target.id){
+        case "rock":
+            userPlay = target.id;
+            computerPlay = getComputerPlay();
+            playGame(userPlay, computerPlay);
+            
+            rounds.textContent = ++rnd;
+
+            break;
+        case "paper":
+            userPlay = target.id;
+            computerPlay = getComputerPlay();
+            playGame(userPlay, computerPlay);
+            rounds.textContent = ++rnd;
+            break;
+        case "scissor":
+            userPlay = target.id;
+            computerPlay = getComputerPlay();
+            playGame(userPlay, computerPlay);
+            rounds.textContent = ++rnd;
+            break;
+    }
+    humanScore.textContent = win;
+    drawScore.textContent = draw;
+    computerScore.textContent = loss;
+
+    if(rnd >= 5){
+        if(win > loss){
+            status_text.textContent = "Congratulations, You Won 👏";
+        } else {
+            status_text.textContent = "Sorry, You Lost! 😵‍💫"
+        }
+        win = 0;
+        draw = 0;
+        loss = 0;
+        rnd = 0;
+    }
+    
+});
